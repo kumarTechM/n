29460a606b5609da716f8abf2c57a9dbb5e4595c
kumarTechM <kumarsarkate94@gmail.com>
2025-05-10 17:59:00 +0530

    Update FindMaxMinFrom3IntNumbers.java

--- src/programming/FindMaxMinFrom3IntNumbers.java
+++ src/programming/FindMaxMinFrom3IntNumbers.java
@@ -20,5 +20,25 @@ public class FindMaxMinFrom3IntNumbers {
 	{
 		maxnumber(1,2,3);
 	}
+	// alternat way for this
+	public static void checkLargerNumber(int a,int b,int c)
+  {
+      int max=0;
+      if(a>=b&& a>=c)
+      {
+          max=a;
+      }
+      else if(b>=a&&b>=c)
+      {
+          max=b;
+          
+      }
+      else
+      {
+          max=c;
+      }
+      System.out.println(max);
+  }
+   
 	
 }

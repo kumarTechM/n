1e54017c0b6c5d4a137a93e83c86afcf44c6d27b
kumarTechM <kumarsarkate94@gmail.com>
2025-05-10 15:13:56 +0530

    Update ReverseSetenceWordUsingSpaceIndex.java

--- src/programming/ReverseSetenceWordUsingSpaceIndex.java
+++ src/programming/ReverseSetenceWordUsingSpaceIndex.java
@@ -36,5 +36,13 @@ public class ReverseSetenceWordUsingSpaceIndex {
 		System.out.println(secondword+" "+firstword);
 		
 	}
+	// alternat waye 
+	String s="abhishek modi";
+		String[] s1=s.split(" ");
+		for(int i=s1.length-1;i>=0;i--)
+		{
+			System.out.print(s1[i]+" ");// o/p--> modi abhishek
+			
+		}
 
 }
